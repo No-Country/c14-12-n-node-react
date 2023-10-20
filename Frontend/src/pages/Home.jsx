0a1acbd42869bf46ff6a9fabf4f4e309a091c8c4
Fddibiasi <fddibiasi@gmail.com>
2023-10-19 21:57:14 -0300

    modificacion package-jason y Home

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -1,25 +1,16 @@
-<<<<<<< HEAD
-import Carrusel from "../components/Carrusel";
+import Carousel from "../components/Carousel";
 import Products from "../components/Products";
 
 const Home = () => {
   return (
     <>
-      <div><Carrusel /></div>
+      <div>
+        <Carousel />
+      </div>
       <div>
         <Products />
       </div>
     </>
-=======
-import Carousel from "../components/Carousel";
-
-const Home = () => {
-  return (
-    <div>
-      <Carousel />
-      <h1>Home Prueba</h1>
-    </div>
->>>>>>> 7fa69012cb32ca9897856a2cf58d95dd4f393fab
   );
 };
 
