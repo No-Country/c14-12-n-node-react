826266eeadd3b879c2b1509268a0438b4455c2af
Fddibiasi <fddibiasi@gmail.com>
2023-10-27 13:10:47 -0300

    responsive heder, carousel y card

--- Frontend/src/components/Header.jsx
+++ Frontend/src/components/Header.jsx
@@ -1,15 +1,29 @@
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 import TopBar from "./TopBar";
 import Menu from "./Menu";
 import NavBar from "../components/NavBar";
 
-
 const Header = () => {
   return (
-    <>
-      <TopBar />
-      <NavBar />
-      <Menu />
-    </>
+    <Container fluid>
+      <Row>
+        <Col>
+          <TopBar />
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <NavBar />
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <Menu />
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -1,12 +1,23 @@
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 import Carousels from "../components/Carousels";
 import Products from "../components/Products";
 
 const Home = () => {
   return (
-    <>
-      <Carousels className="carousel" />
-      <Products />
-    </>
+    <Container fluid>
+      <Row>
+        <Col>
+          <Carousels className="carousel" />
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <Products />
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
