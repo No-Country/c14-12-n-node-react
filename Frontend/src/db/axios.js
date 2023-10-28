b54a4705ac9415d4f939eae0b1e68499f9ce33a6
Map4che <ljuan648l@gmail.com>
2023-10-28 15:42:04 -0500

    Actualizacion consulta Sales

--- Frontend/src/db/axios.js
+++ Frontend/src/db/axios.js
@@ -3,8 +3,8 @@ import axios from "axios";
 const instance = axios.create({
   baseURL: "http://localhost:4000/api",
   withCredentials: true,
-  //baseURL: 'https://marlui-map4che.koyeb.app/api',
-  //withCredentials:true
+  //baseURL: "https://marlui-map4che.koyeb.app/api",
+  //withCredentials: true,
 });
 
 export default instance;
