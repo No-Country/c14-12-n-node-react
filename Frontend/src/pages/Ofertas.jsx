4d365930f2c7a9d276294500afcef9e12657c44f
Fddibiasi <fddibiasi@gmail.com>
2023-10-28 18:10:57 -0300

    que belleza

--- Frontend/src/pages/Ofertas.jsx
+++ Frontend/src/pages/Ofertas.jsx
@@ -1,15 +1,52 @@
-
-
+import { useState, useEffect } from "react";
+import { getSales } from "../Services/productosServices";
+import Product from "../components/Product";
+import Spinner from "react-bootstrap/esm/Spinner";
 
 const Ofertas = () => {
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(true);
 
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const data = await getSales();
+        setProducts(data);
+        setLoading(false);
+      } catch (error) {
+        console.log("Error fetching products", error);
+      }
+    };
+    fetchData();
+  }, []);
 
+  if (loading) {
+    return (
+      <div className="d-flex align-items-center">
+        <Spinner animation="grow" />
+      </div>
+    );
+  } else {
+    // limita la cantidad de tarjetas a 4
+    const limitedProducts = products.slice(0,3);
 
-  return (
-    <div>
-      <h1>Ofertas</h1>
-    </div>
-  );
-};
+    return (
+      <div
+        className="container"
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          width: "100%",
+          height: "639px",
+          gap: "80px",
+        }}
+      >
+        {limitedProducts.map((product) => (
+          <Product key={product?._id} {...product} />
+        ))}
+      </div>
+    );
+  }
+}
 
 export default Ofertas;
