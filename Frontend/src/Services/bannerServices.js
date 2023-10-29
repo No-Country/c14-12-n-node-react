70e39d3188a5b3af16e9f40e731c788cd40e4e2a
Map4che <113218333+Map4che@users.noreply.github.com>
2023-10-29 17:27:40 -0500

    Update bannerServices.js

--- Frontend/src/Services/bannerServices.js
+++ Frontend/src/Services/bannerServices.js
@@ -1,5 +1,5 @@
-const URL = "http://localhost:4000/api";
-
+//const URL = "http://localhost:4000/api";
+const URL = "https://marlui-map4che.koyeb.app/api";
 export async function getBanner() {
   return await fetch(`${URL}/banner`).then((res) => res.json());
 }
